9d7a2d0b2f4850a4e14bb3c80c5988b0caa3560f
Andrew <osterandrew2@gmail.com>
2024-11-12 11:37:31 -0500

    added plugin functionality

--- Assets/InvincibilityPlugin.cs
+++ Assets/InvincibilityPlugin.cs
@@ -1,6 +1,21 @@
 using UnityEngine;
+using UnityDLL;
 
 public class InvincibilityPlugin : MonoBehaviour
 {
 
+    [SerializeField]
+    InClass Player;
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Player.SetInv(true);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            Player.SetInv(false);
+        }
+    }
 }
